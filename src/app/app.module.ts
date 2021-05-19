ddacdf8f6c8961958caf1421ec8fc94f16b90c2e
Chris Hamilton <chris_h_27@hotmail.ca>
2021-05-19 18:19:49 -0400

    Upodate main commands-ui component and app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,46 @@
-import {LayoutModule} from '@angular/cdk/layout';
-import {HttpClientModule} from '@angular/common/http';
-import {NgModule} from '@angular/core';
-import {FlexLayoutModule} from '@angular/flex-layout';
-import {FormsModule} from '@angular/forms';
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { LayoutModule } from "@angular/cdk/layout";
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import * as Sentry from '@sentry/browser';
-import {MarkdownModule, MarkedOptions} from 'ngx-markdown';
-import {MonacoEditorModule} from 'ngx-monaco-editor';
+import * as Sentry from "@sentry/browser";
+import { MarkdownModule, MarkedOptions } from "ngx-markdown";
+import { MonacoEditorModule } from "ngx-monaco-editor";
 
-import {AppRoutingModule} from './app-routing.module';
+import { AppRoutingModule } from "./app-routing.module";
 
-import {AppComponent} from './app.component';
-import {CheatsheetsModule} from './cheatsheets/cheatsheets.module';
-import {CommandDisplayComponent} from './commands/command-display/command-display.component';
-import {CommandsComponent} from './commands/commands.component';
-import {ErrorComponent} from './error/error.component';
-import {FooterComponent} from './footer/footer.component';
-import {FeatureBoxComponent} from './home/feature-box/feature-box.component';
-import {HomeComponent} from './home/home.component';
-import {HomebrewSharingModule} from './homebrew-sharing/homebrew-sharing.module';
-import {LoginModule} from './login/login.module';
-import {MaterialModule} from './material/material.module';
-import {NavbarComponent} from './navbar/navbar.component';
-import {AutomationEditorModule} from './shared/automation-editor/automation-editor.module';
-import {DialogsModule} from './shared/dialogs/dialogs.module';
-import {DiscordEmbedModule} from './shared/discord-embed/discord-embed.module';
-import {registerDraconicLanguage} from './shared/monacoDraconic';
-import {ThemesModule} from './themes/themes.module';
-import { CommandsUiComponent } from './commands-ui/commands-ui.component';
+import { AppComponent } from "./app.component";
+import { CheatsheetsModule } from "./cheatsheets/cheatsheets.module";
+import { CommandDisplayComponent } from "./commands/command-display/command-display.component";
+import { CommandsComponent } from "./commands/commands.component";
+import { ErrorComponent } from "./error/error.component";
+import { FooterComponent } from "./footer/footer.component";
+import { FeatureBoxComponent } from "./home/feature-box/feature-box.component";
+import { HomeComponent } from "./home/home.component";
+import { HomebrewSharingModule } from "./homebrew-sharing/homebrew-sharing.module";
+import { LoginModule } from "./login/login.module";
+import { MaterialModule } from "./material/material.module";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { AutomationEditorModule } from "./shared/automation-editor/automation-editor.module";
+import { DialogsModule } from "./shared/dialogs/dialogs.module";
+import { DiscordEmbedModule } from "./shared/discord-embed/discord-embed.module";
+import { registerDraconicLanguage } from "./shared/monacoDraconic";
+import { ThemesModule } from "./themes/themes.module";
+import { CommandsUiComponent } from "./commands-ui/commands-ui.component";
+import { ButtonComponent } from "./commands-ui/button/button.component";
+import { MatButtonModule } from "@angular/material/button";
+import { ModuleButtonsComponent } from "./commands-ui/module-buttons/module-buttons.component";
+import { CommandButtonsComponent } from './commands-ui/command-buttons/command-buttons.component';
+import { SubcommandButtonsComponent } from './commands-ui/command-buttons/subcommand-buttons/subcommand-buttons.component';
+import { PrimaryArgButtonsComponent } from './commands-ui/primary-arg-buttons/primary-arg-buttons.component';
+import { OutputAreaComponent } from './commands-ui/output-area/output-area.component';
 
-Sentry.init({dsn: 'https://af2b06560981446bb55f64b6f79fd520@sentry.io/1486249'});
+Sentry.init({
+  dsn: "https://af2b06560981446bb55f64b6f79fd520@sentry.io/1486249",
+});
 
 const markdownConfig = {
   markedOptions: {
@@ -47,7 +56,7 @@ const monacoConfig = {
   // baseUrl: './assets/monaco/vs',
   onMonacoLoad: () => {
     registerDraconicLanguage((<any>window).monaco);
-  }
+  },
 };
 
 @NgModule({
@@ -61,9 +70,15 @@ const monacoConfig = {
     CommandsComponent,
     CommandDisplayComponent,
     CommandsUiComponent,
+    ButtonComponent,
+    ModuleButtonsComponent,
+    CommandButtonsComponent,
+    SubcommandButtonsComponent,
+    PrimaryArgButtonsComponent,
+    OutputAreaComponent,
   ],
   imports: [
-    BrowserModule.withServerTransition({appId: 'serverApp'}),
+    BrowserModule.withServerTransition({ appId: "serverApp" }),
     HttpClientModule,
     BrowserAnimationsModule,
     LayoutModule,
@@ -79,10 +94,10 @@ const monacoConfig = {
     AppRoutingModule,
     ThemesModule,
     LoginModule,
-    MonacoEditorModule.forRoot(monacoConfig)
+    MonacoEditorModule.forRoot(monacoConfig),
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
--- src/app/commands-ui/commands-ui.component.ts
+++ src/app/commands-ui/commands-ui.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 import { Meta } from "@angular/platform-browser";
 import { environment } from "../../environments/environment";
 import { COMMAND_MODULES } from "../command-data/CommandModules";
+import { sortByName } from "./constants";
 import {
   CommandModule,
   Command,
@@ -12,6 +13,7 @@ import {
   TargetArgument,
   Category,
   ValueType,
+  Argument,
 } from "../schemas/Commands";
 
 @Component({
@@ -26,7 +28,9 @@ export class CommandsUiComponent implements OnInit {
   modules: CommandModule[] = COMMAND_MODULES;
   activeModule: CommandModule;
   activeCommand: Command;
-  activeSubcommand: Command;
+  activeSubcommand: Subcommand;
+  activePrimaryArgs: PrimaryArgument[] = [];
+  activeSecondaryArgs: SecondaryArgument[] = [];
 
   constructor(private meta: Meta) {
     this.meta.updateTag({ name: "description", content: this.description });
@@ -43,15 +47,55 @@ export class CommandsUiComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  setModule(module: CommandModule): void {
+  setActiveModule(module: CommandModule) {
     this.activeModule = module;
+    this.activeCommand = null;
+    this.activeSubcommand = null;
+    this.activePrimaryArgs = [];
+    this.activeSecondaryArgs = [];
   }
 
-  setCommand(command: Command): void {
+  setActiveCommand(command: Command) {
     this.activeCommand = command;
+    this.activeSubcommand = null;
+    this.activePrimaryArgs = this.getRequiredArgs();
+    this.activeSecondaryArgs = [];
   }
 
-  setSubcommand(subcommand: Command): void {
+  setActiveSubcommand(subcommand: Subcommand) {
     this.activeSubcommand = subcommand;
+    this.activePrimaryArgs = this.getRequiredArgs();
+    this.activeSecondaryArgs = [];
+  }
+
+  setActivePrimaryArgs(primaryArgs: PrimaryArgument[]) {
+    this.activePrimaryArgs = primaryArgs;
+    this.activeSecondaryArgs = [];
+  }
+
+  getModules(): CommandModule[] {
+    return this.modules.sort(sortByName);
+  }
+
+  getCommands(): Command[] {
+    return this.activeModule
+      ? this.activeModule.commands.sort(sortByName)
+      : null;
+  }
+
+  getPrimaryArguments(): PrimaryArgument[] {
+    if (this.activeSubcommand)
+      return this.activeSubcommand.primaryArgs.sort(sortByName);
+    else if (this.activeCommand)
+      return this.activeCommand.primaryArgs.sort(sortByName);
+    else return [];
+  }
+
+  getRequiredArgs(): PrimaryArgument[] {
+    let requiredArgs = [];
+    for (let arg of this.getPrimaryArguments()) {
+      if (arg.required) requiredArgs.push(arg);
+    }
+    return requiredArgs;
   }
 }
