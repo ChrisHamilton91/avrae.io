afab0c5e9a62ca0877f379cde528e4b200646e6f
Chris Hamilton <chris_h_27@hotmail.ca>
2021-07-12 03:40:21 -0400

    peel off more stuff

--- src/main.server.ts
+++
@@ -1,5 +0,0 @@
-import { enableProdMode } from "@angular/core";
-
-export { AppServerModule } from "./app/app.server.module";
-export { renderModule, renderModuleFactory } from "@angular/platform-server";
-// enableProdMode();
