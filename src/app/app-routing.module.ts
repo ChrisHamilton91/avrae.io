e1b6f19efe2135a56dd567ff84123085ea90fcbf
Chris Hamilton <chris_h_27@hotmail.ca>
2021-07-15 23:46:27 -0400

    Fix routing issues

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { CommandsUiComponent } from "./commands-ui/commands-ui.component";
 
-const routes: Routes = [{ path: "", component: CommandsUiComponent }];
+const routes: Routes = [{ path: "**", component: CommandsUiComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { relativeLinkResolution: "legacy" })],
