3e234a065f72d0302f408dd9d22aade16d2572bc
Chris Hamilton <chris_h_27@hotmail.ca>
2021-10-03 02:37:52 -0400

    Add arguments to roll command

--- src/app/command-data/original-modules/dice/roll.ts
+++ src/app/command-data/original-modules/dice/roll.ts
@@ -1,4 +1,9 @@
-import { Command, PrimaryArgument, ValueType } from "../../command-schema";
+import {
+  Command,
+  PrimaryArgument,
+  SecondaryArgument,
+  ValueType,
+} from "../../command-schema";
 
 const ROLLSTR = new PrimaryArgument({
   name: "roll string",
@@ -8,12 +13,33 @@ const ROLLSTR = new PrimaryArgument({
   desc: "The roll string (ie. 1d6, 2d4+3, 2d10kh1).",
 });
 
+const NAME = new PrimaryArgument({
+  name: "name",
+  signature: "name",
+  required: false,
+  valueType: ValueType.STRING,
+  desc: "The name of the dice roll.",
+});
+
+const ADV = new SecondaryArgument({
+  name: "advantage",
+  cmdString: "adv",
+  valueType: ValueType.TRUE,
+  desc: "Give advantage to the roll(s)",
+});
+
+const DIS = new SecondaryArgument({
+  name: "disadvantage",
+  cmdString: "dis",
+  valueType: ValueType.TRUE,
+  desc: "Give disadvantage to the roll(s)",
+});
+
 export const ROLL = new Command({
   name: "Dice Roll",
   cmdStrings: ["roll", "r"],
   shortDesc:
-    "Roll any combination of dice in the XdY format. Multiple rolls can be added together as an equation. " +
-    "Standard Math operators and Parentheses can be used: () + - / *. Roll also accepts adv and dis for Advantage and Disadvantage. " +
-    "Rolls can also be tagged with [text] for informational purposes. Any text after the roll will assign the name of the roll.",
-  primaryArgs: [ROLLSTR],
+    "Roll any combination of dice in the XdY format. Multiple rolls can be added together as an equation.",
+  primaryArgs: [ROLLSTR, NAME],
+  secondaryArgs: [ADV, DIS],
 });
